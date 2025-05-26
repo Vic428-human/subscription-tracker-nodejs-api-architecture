8ec3a8d1148efaa1358280d589c68a84f903780e
Vic428-human <z0983195379a@gmail.com>
2025-05-27 00:04:16 +0800

    /sign-in

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,14 +1,12 @@
 import { Router } from 'express';
+import { signIn } from '../controllers/auth.controller.js'; 
 
 const authRouter = Router(); 
 
 // ex: /api/v1/auth/sign-up
-authRouter.post('/sign-up' , (req, res) => {
-    // 這邊會放 handler，處理重複且相同的邏輯
-    res.send({title:'sign-up'})
-});
-authRouter.post('/sign-in' , (req, res) => res.send({title:'sign-in'}));
-authRouter.post('/sign-out' , (req, res) => res.send({title:'sign-out'}));
+// authRouter.post('/sign-up' , signUp);
+authRouter.post('/sign-in' , signIn);
+// authRouter.post('/sign-out' , signOut);
 
 export default authRouter;
 
